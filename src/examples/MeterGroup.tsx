79a1da2324ea5feff9d621a5445bf4bafede05cb
Aaron <aaron.wang@wexinc.com>
2025-10-27 17:50:04 -0400

    updated metergroup demo code

--- src/examples/MeterGroup.tsx
+++ src/examples/MeterGroup.tsx
@@ -32,6 +32,40 @@ export default function MeterGroupDemo() {
                     ))}
                 </MeterGroup.Labels>
             </MeterGroup>
+
+            {/* Color
+            MeterGroup.Meter and MeterGroup.Marker components supports custom color values. Use color property or pass color with className or style. color has custom color names like blue, emerald, violet, amber, etc. or hex, rgb, hsl, or hsla values. */}
+            <MeterGroup>
+                <MeterGroup.Meters>
+                    <MeterGroup.Meter value={12} color="violet" />
+                    <MeterGroup.Meter value={14} color="#10B981" />
+                    <MeterGroup.Meter value={10} color="rgb(244, 63, 94)" />
+                    <MeterGroup.Meter value={8} className="bg-blue-500" />
+                    <MeterGroup.Meter value={10} style={{ backgroundColor: '#EAB308' }} />
+                </MeterGroup.Meters>
+                <MeterGroup.Labels>
+                    <MeterGroup.Label>
+                        <MeterGroup.Marker color="violet" />
+                        <MeterGroup.Text>Violet</MeterGroup.Text>
+                    </MeterGroup.Label>
+                    <MeterGroup.Label>
+                        <MeterGroup.Marker color="#10B981" />
+                        <MeterGroup.Text>Emerald</MeterGroup.Text>
+                    </MeterGroup.Label>
+                    <MeterGroup.Label>
+                        <MeterGroup.Marker color="rgb(244, 63, 94)" />
+                        <MeterGroup.Text>Rose</MeterGroup.Text>
+                    </MeterGroup.Label>
+                    <MeterGroup.Label>
+                        <MeterGroup.Marker className="bg-blue-500" />
+                        <MeterGroup.Text>Blue</MeterGroup.Text>
+                    </MeterGroup.Label>
+                    <MeterGroup.Label>
+                        <MeterGroup.Marker style={{ backgroundColor: '#EAB308' }} />
+                        <MeterGroup.Text>Yellow</MeterGroup.Text>
+                    </MeterGroup.Label>
+                </MeterGroup.Labels>
+            </MeterGroup>
         </div>
     );
 }
